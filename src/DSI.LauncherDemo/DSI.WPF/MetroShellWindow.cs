b8e200a92e9cc174d825db7746060fa3ce906494
Mateus Mauricio <mateus10.m@live.com>
2021-01-26 09:46:42 -0300

    New: Criação de uma base Shell personalizada com suporte há prism e mahapps.

---
+++ src/DSI.LauncherDemo/DSI.WPF/MetroShellWindow.cs
@@ -0,0 +1,10 @@
+﻿using MahApps.Metro.Controls;
+using Prism.Services.Dialogs;
+
+namespace DSI.WPF
+{
+    public class MetroShellWindow : MetroWindow, IDialogWindow
+    {
+        public IDialogResult Result { get; set; }
+    }
+}
